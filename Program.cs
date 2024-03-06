3c129b250554e10e19f863f3ebc3e5badaec4436
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-03-06 16:40:07 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Warrecs_2_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryReader br = new BinaryReader(File.OpenRead(args[0]));
+            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "//" + Path.GetExtension(args[0]));
+            int i = 0;
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                using FileStream FS = File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetExtension(args[0]) + "//" + i);
+                BinaryWriter bw = new(FS);
+                var variable = br.ReadBytes(4);
+                System.Array.Reverse(variable);
+                int size = System.BitConverter.ToInt32(variable, 0);
+                bw.Write(br.ReadBytes(size));
+                bw.Close();
+                i++;
+            }
+        }
+    }
+}
